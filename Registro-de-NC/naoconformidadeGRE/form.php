00ed854693be9a84867cfffce67b6c0160d94337
João Vitor Cidral Varpechoski <119749953+joaovitorcidralv@users.noreply.github.com>
2023-10-30 21:46:35 -0300

    Update form.php

--- Registro-de-NC/naoconformidadeGRE/form.php
+++ Registro-de-NC/naoconformidadeGRE/form.php
@@ -1,7 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $resp = $_POST['resp'];
+    $resp_email = $_POST['resp_email'];
     $rqaresp = $_POST['rqaresp'];
+    $rqaresp_email = $_POST['rqaresp_email'];
     $inicio = $_POST['inicio'];
     $final = $_POST['final'];
 ?>
@@ -25,29 +27,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <input type="radio" name="quest02" id="q_sim" value="sim" required><label for="q_sim">Sim</label>
             <input type="radio" name="quest02" id="q_nao" value="nao" required><label for="q_nao">Não</label>
             <label for="quest02b">Os fornecedores estão em acordo com a equipe técnica? </label>
-            <input type="radio" name="quest02b" id="q_sim" value="sim" required><label for="q_sim">Sim</label>
+            <input type="radio" name="quest02b" id="q_sim" value="sim" required><label for "q_sim">Sim</label>
             <input type="radio" name="quest02b" id="q_nao" value="nao" required><label for="q_nao">Não</label>
+            <label for="resp_email">Email do Responsável: </label>
+            <input type="email" name="resp_email" id="resp_email" required>
+            <label for="rqaresp_email">Email do RQA Responsável: </label>
+            <input type="email" name="rqaresp_email" id="rqaresp_email" required>
             <label for="quest03">Os produtos de trabalho foram considerados no planejamento? </label>
-            <input type="radio" name="quest03" id="q_sim" value="sim" required><label for="q_sim">Sim</label>
-            <input type="radio" name="quest03" id="q_nao" value="nao" required><label for="q_nao">Não</label>
-            <label for="quest03b">Os requisitos estão alinhados com os produtos de trabalho disponíveis? </label>
-            <input type="radio" name="quest03b" id="q_sim" value="sim" required><label for="q_sim">Sim</label>
-            <input type="radio" name="quest03b" id="q_nao" value="nao" required><label for="q_nao">Não</label>
-            <label for="quest04">Os requisitos são consistentes em sua maioria?</label>
-            <input type="radio" name="quest04" id="q_sim" value="sim" required><label for="q_sim">Sim</label>
-            <input type="radio" name="quest04" id="q_nao" value="nao" required><label for="q_nao">Não</label>
-            <label for="quest04b">Uma revisão periódica dos requisitos está sendo feita?</label>
-            <input type="radio" name="quest04b" id="q_sim" value="sim" required><label for="q_sim">Sim</label>
-            <input type="radio" name="quest04b" id="q_nao" value="nao" required><label for="q_nao">Não</label>
-            <label for="quest05">Mudanças de requisitos são registradas?</label>
-            <input type="radio" name="quest05" id="q_sim" value="sim" required><label for="q_sim">Sim</label>
-            <input type="radio" name="quest05" id="q_nao" value="nao" required><label for="q_nao">Não</label>
-            <label for="quest05b">É analisado o impacto dessas mudanças no projeto?</label>
-            <input type="radio" name="quest05b" id="q_sim" value="sim" required><label for="q_sim">Sim</label>
-            <input type="radio" name="quest05b" id="q_nao" value="nao" required><label for="q_nao">Não</label>
             <button type="submit">Verificar Resultados</button>
             <input type="hidden" name="resp" value="<?php echo $resp; ?>">
             <input type="hidden" name="rqaresp" value="<?php echo $rqaresp; ?>">
+            <input type="hidden" name="resp_email" value="<?php echo $resp_email; ?>"> 
+            <input type="hidden" name="rqaresp_email" value="<?php echo $rqaresp_email; ?>"> 
             <input type="hidden" name="inicio" value="<?php echo $inicio; ?>">
             <input type="hidden" name="final" value="<?php echo $final; ?>">
         </form>
